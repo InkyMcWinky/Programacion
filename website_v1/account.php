7c3637b19c12861cfd48e780caf2cef9e2f3050e
invaderzon <159202214+invaderzon@users.noreply.github.com>
2024-05-10 18:23:48 -0600

    Página de ajustes de usuario

--- website_v1/account.php
+++ website_v1/account.php
@@ -11,51 +11,11 @@
     <div class="divided">
         <nav id="menuAcc" class="menu-a">
             <div id="btnOrders" class="myitem">Orders</div>
-            <div id="btnSales" class="myitem">Sales</div>
-            <div id="btnQuotes" class="myitem">Pricing requests</div>
-            <div id="btnInvoices" class="myitem">Invoices</div>
-            <div id="btnPayments" class="myitem">Payments</div>
+
             <div id="btnSettings" class="myitem">Settings</div>
         </nav>
         <div id="info-c">
-
-            
-
-            <table id="sales" class="info-client">
-                <tr>
-                    <th>Sale #</th>
-                    <th>Series</th>
-                    <th>Number</th>
-                    <th>Date</th>
-                    <th>Currency</th>
-                    <th>Total</th>
-                    <th>Departures</th>
-                    <th>Status</th>
-                </tr>
-                <tr>
-                    <td>145245</td>
-                    <td>OCC</td>
-                    <td>285</td>
-                    <td>04/20/2023</td>
-                    <td>USD</td>
-                    <td>$1, 204.25</td>
-                    <td>2</td>
-                    <td>Pending</td>
-                </tr>
-                <tr>
-                    <td>145245</td>
-                    <td>OCC</td>
-                    <td>285</td>
-                    <td>04/27/2023</td>
-                    <td>USD</td>
-                    <td>$1, 204.25</td>
-                    <td>2</td>
-                    <td>Pending</td>
-                </tr>
-
-            </table>
-
-            <table id="orders" class="info-client">
+            <table id="orders" class="info-client escondido">
                 <tr>
                     <th>Order #</th>
                     <th>Series</th>
@@ -89,151 +49,82 @@
 
             </table>
 
-            <table id="quotes" class="info-client">
-                <tr>
-                    <th>Order #</th>
-                    <th>Series</th>
-                    <th>Number</th>
-                    <th>Date</th>
-                    <th>Currency</th>
-                    <th>Total</th>
-                    <th>Departures</th>
-                    <th>Status</th>
-                </tr>
-                <tr>
-                    <td>145245</td>
-                    <td>OCC</td>
-                    <td>285</td>
-                    <td>04/17/2023</td>
-                    <td>USD</td>
-                    <td>$1, 204.25</td>
-                    <td>2</td>
-                    <td>Pending</td>
-                </tr>
-                <tr>
-                    <td>145245</td>
-                    <td>OCC</td>
-                    <td>285</td>
-                    <td>04/17/2023</td>
-                    <td>USD</td>
-                    <td>$1, 204.25</td>
-                    <td>2</td>
-                    <td>Pending</td>
-                </tr>
-
-            </table>
-
-            <table id="invoices" class="info-client">
-                <tr>
-                    <th>Order #</th>
-                    <th>Series</th>
-                    <th>Number</th>
-                    <th>Date</th>
-                    <th>Currency</th>
-                    <th>Total</th>
-                    <th>Departures</th>
-                    <th>Status</th>
-                </tr>
-                <tr>
-                    <td>145245</td>
-                    <td>OCC</td>
-                    <td>285</td>
-                    <td>04/17/2023</td>
-                    <td>USD</td>
-                    <td>$1, 204.25</td>
-                    <td>2</td>
-                    <td>Pending</td>
-                </tr>
-                <tr>
-                    <td>145245</td>
-                    <td>OCC</td>
-                    <td>285</td>
-                    <td>04/17/2023</td>
-                    <td>USD</td>
-                    <td>$1, 204.25</td>
-                    <td>2</td>
-                    <td>Pending</td>
-                </tr>
-
-            </table>
-
-            <table id="payments" class="info-client">
-                <tr>
-                    <th>Order #</th>
-                    <th>Series</th>
-                    <th>Number</th>
-                    <th>Date</th>
-                    <th>Currency</th>
-                    <th>Total</th>
-                    <th>Departures</th>
-                    <th>Status</th>
-                </tr>
-                <tr>
-                    <td>145245</td>
-                    <td>OCC</td>
-                    <td>285</td>
-                    <td>04/17/2023</td>
-                    <td>USD</td>
-                    <td>$1, 204.25</td>
-                    <td>2</td>
-                    <td>Pending</td>
-                </tr>
-                <tr>
-                    <td>145245</td>
-                    <td>OCC</td>
-                    <td>285</td>
-                    <td>04/17/2023</td>
-                    <td>USD</td>
-                    <td>$1, 204.25</td>
-                    <td>2</td>
-                    <td>Pending</td>
-                </tr>
-
-            </table>
-            
-            <table id="settings" class="info-client">
-                <tr>
-                    <th>Sale </th>
-                    <th>Series</th>
-                    <th>Number</th>
-                    <th>Date</th>
-                    <th>Currency</th>
-                    <th>Total</th>
-                    <th>Departures</th>
-                    <th>Status</th>
-                </tr>
-                <tr>
-                    <td>145245</td>
-                    <td>OCC</td>
-                    <td>285</td>
-                    <td>04/20/2023</td>
-                    <td>USD</td>
-                    <td>$1, 204.25</td>
-                    <td>2</td>
-                    <td>Pending</td>
-                </tr>
-                <tr>
-                    <td>145245</td>
-                    <td>OCC</td>
-                    <td>285</td>
-                    <td>04/27/2023</td>
-                    <td>USD</td>
-                    <td>$1, 204.25</td>
-                    <td>2</td>
-                    <td>Pending</td>
-                </tr>
-
-            </table>
-            
-            <div class="none">
+            <div class="none escondido">
                 <div class="logo-fondo"></div>
                 <h3>You don't have any <span>orders</span> at the moment...</h3>
             </div>
 
+            <div id="set" class="settings">
+                <h3>User information</h3>
+                <table>
+                    <tr>
+                        <td>
+                            <h4>Name:</h4>
+                        </td>
+                        <td>
+                            <p>Name Last Name</p>
+                        </td>
+                        <td><button id="btnChName">Change name</button></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <h4>Shipping address:</h4>
+                        </td>
+                        <td>
+                            <p>A Place 5618, State, USA</p>
+                        </td>
+                        <td><button id="btnChAddress">Change address</button></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <h4>Phone number:</h4>
+                        </td>
+                        <td>
+                            <p>+52 6484 15641</p>
+                        </td>
+                        <td><button id="btnChPhNum">Change phone number</button></td>
+                    </tr>
+                </table>
+
+                <br>
+
+                <h3>Account Settings</h3>
+                <table>
+                    <tr>
+                        <td>
+                            <h4>User name:</h4>
+                        </td>
+                        <td>
+                            <p>user_name</p>
+                        </td>
+                        <td><button id="btnChUser">Change username</button></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <h4>User email:</h4>
+                        </td>
+                        <td>
+                            <p>anemail@example.com</p>
+                        </td>
+                        <td><button id="btnChEmail">Change email</button></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <h4>Password:</h4>
+                        </td>
+                        <td>
+                            <p>*********</p>
+                        </td>
+                        <td><button id="btnChPassword">Change Password</button></td>
+                    </tr>
+
+                </table>
+                <br><br><br>
+
+                <button id="btnDeleteAcc">Delete account</button>
+
+            </div>
         </div>
-
     </div>
-
-
 
 </div>
