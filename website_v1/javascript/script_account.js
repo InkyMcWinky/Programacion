c4da805bed7880d3dbe1504b82b085f51b507ba5
al211185 <92053617+al211185@users.noreply.github.com>
2024-05-11 00:36:27 -0600

    Add files via upload

---
+++ website_v1/javascript/script_account.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    // Suponiendo que obtienes el nombre de usuario de alguna fuente, por ejemplo, una variable llamada nombreUsuario
+    var nombreUsuario = "UsuarioEjemplo";
+
+    // Actualizar el contenido del elemento h2 con el id "client-name" con el nombre de usuario obtenido
+    $('#client-name').text(nombreUsuario);
+})
